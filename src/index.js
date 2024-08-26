ceadb8f0fa554ab39b6c776e32eb15d9bc728366
泉户泽雨 <1624910218@qq.com>
2024-08-26 15:45:06 +0800

    fix: 回滚版本

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MemeGenerator = void 0;
-const petpet_1 = require("@src/memeGenerator/petpet");
-const Petpet = petpet_1.Petpet.genPetpetGif;
-exports.MemeGenerator = { Petpet };
--- src/tools/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tools = void 0;
-const cropToCircle_1 = require("./cropToCircle");
-const gifTools_1 = require("@src/tools/gifTools");
-// 圆形裁切工具集
-exports.tools = { CropToCircle: cropToCircle_1.CropToCircle, saveGifToFile: gifTools_1.saveGifToFile };
