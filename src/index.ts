7017bd805695ee3427e20bf69973087459258562
泉户泽雨 <1624910218@qq.com>
2024-09-04 01:14:44 +0800

    ``` 插件 memelib 配置和应用调整
    - 添加了 `SharpService` 类型的导入，以支持新的插件依赖。
    - 实现了 `inject` 函数，指明了对 'QhzySharp' 插件的依赖。
    - 定义了 `pluginSharp` 变量以保存来自 'QhzySharp' 插件的服务实例。
    - 扩展了 `Config` 接口，包含了 'hello' 字符串类型配置项。
    - 更新了 `Config` Schema，添加了 'hello' 配置项，默认值为 'hello world'。
    - 在 `apply` 函数中初始化 `pluginSharp` 变量，以使用 'QhzySharp' 插件的服务。
    
    这些更改实现了对插件依赖注入的支持，并为 memelib 插件的配置和应用提供了新的功能。
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { Context, Schema } from 'koishi'
 
+import type { SharpService } from "@quanhuzeyu/koishi-plugin-qhzy-sharp"
+
 export const name = 'memelib'
+export const inject = {
+	required: ['QhzySharp']
+}
+export let pluginSharp:SharpService
 
-export interface Config {}
+export interface Config {
+	hello: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+	hello: Schema.string().default('hello world'),
+})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+	pluginSharp = ctx.QhzySharp
 }
