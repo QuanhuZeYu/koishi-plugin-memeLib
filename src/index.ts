eccbd1638a341551dd98dc75899ac5049b7e1f7a
泉户泽雨 <1624910218@qq.com>
2024-09-04 02:31:16 +0800

    失败

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import { Context, Schema } from 'koishi'
-
-import type { SharpService } from "@quanhuzeyu/koishi-plugin-qhzy-sharp"
-import path from 'path'
-
-export const name = 'memelib'
-export const baseDir = path.resolve(__dirname)
-export const inject = {
-	required: ['QhzySharp']
-}
-export let pluginSharp:SharpService
-
-export interface Config {
-	hello: string
-}
-
-export const Config: Schema<Config> = Schema.object({
-	hello: Schema.string().default('hello world'),
-})
-
 export function apply(ctx: Context) {
-	pluginSharp = ctx.QhzySharp
+	// ctx.plugin(MemeGeneratorService)
 }
