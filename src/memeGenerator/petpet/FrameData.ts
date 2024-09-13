192a544becb46d132f81b9f8491fd786157c0c33
泉户泽雨 <1624910218@qq.com>
2024-09-13 23:41:30 +0800

    refactor(petpet): 优化petpet

--- src/memeGenerator/petpet/FrameData.ts
+++ src/memeGenerator/petpet/FrameData.ts
@@ -30,51 +30,6 @@ export const frames: FrameData[] = [
 
 
 /**
- * 合成图片的异步函数
- * 将输入图片（inputImg）缩放并合成到背景图片（handImg）上
- * 
- * @param input - 原始输入图片数据
- * @param hand - 背景图片数据
- * @param frameData - 包含缩放和合成参数的对象
- * @returns 合成后的图片数据（Buffer）
- */
-export const createFrame = timeIt(
-    async function createFrame(
-    input: Buffer,
-    hand: Buffer,
-    frameData: FrameData,
-) {
-    // 输入验证
-    if (!Buffer.isBuffer(input) || !Buffer.isBuffer(hand)) {
-        throw new Error('输入的图像数据必须是 Buffer 类型');
-    }
-
-    if (
-        typeof frameData.width !== 'number' ||
-        typeof frameData.height !== 'number' ||
-        typeof frameData.x !== 'number' ||
-        typeof frameData.y !== 'number'
-    ) {
-        throw new Error('FrameData 参数无效，必须包含数字类型的 width、height、x 和 y 属性');
-    }
-
-    try {
-        const join:ComposeJoin[] = [{
-            img: input,
-            frameData:frameData
-        }]
-        // 确保输入图片透明背景
-        const resizedInputImg = await tools.gifTools.compose(hand,join)
-        return resizedInputImg
-    } catch (err) {
-        // 错误处理
-        logger.error('创建帧时发生错误:', err);
-        throw err;
-    }
-}, false)
-
-
-/**
  * 异步加载手部图像数组
  * 
  * 此函数接收一个FrameData数组作为参数，但实际不使用这些数据来生成手部图像而是通过索引
@@ -115,14 +70,6 @@ export const loadHandImages = timeIt(async function loadHandImages(): Promise<Bu
     }
 })
 
-const craftPng2Gif = timeIt(async function generateGif(
-    frameBuffers: Buffer[], 
-    fps?: number
-): Promise<Buffer> {
-    const gifBuf = tools.gifTools.pngsToGifBuffer_ffmpeg(frameBuffers)
-    return gifBuf
-});
-
 /**
  * 异步函数：生成动图GIF
  * @param inputImg 图像缓冲区，单张图片或者是GIF
@@ -132,80 +79,52 @@ const craftPng2Gif = timeIt(async function generateGif(
  * 最后将所有帧合并生成GIF图像如果在生成过程中发生错误，会抛出异常并打印错误信息
  */
 const craftPetpetGif =  timeIt(
-    async function genPetpetGif(inputImg: Buffer, isGif: boolean = false): Promise<Buffer | void> {
-    if (!isGif) {
-        // 处理静态图像
-        const frameBuffers = await processStaticImage(inputImg);
-        return frameBuffers ? craftPng2Gif(frameBuffers) : undefined;
-    } else {
-        // 处理 GIF 图像
-        const frameBuffers = await processGifImage(inputImg);
-        return frameBuffers ? craftPng2Gif(frameBuffers) : undefined;
+    async function genPetpetGif(inputImg: Buffer): Promise<Buffer | void> {
+    const isGif = await tools.imageTools.isGif(inputImg)
+    if(!isGif) {
+        return await processStaticImage(inputImg)
+    } else if(isGif) {
+        return await processGifImage(inputImg)
     }
 })
 
 // 处理静态图像
-async function processStaticImage(inputImg: Buffer): Promise<Buffer[] | undefined> {
+const processStaticImage = timeIt(async function processStaticImage(inputImg: Buffer): Promise<Buffer> {
+    const result:Buffer[] = []  // 结果缓冲区
     // 读取手部图
     const hands = await loadHandImages()
-
-    // 1.合成图像
-    const frameBuffers = await Promise.all(
-        frames.map((frameData, index) => createFrame(inputImg, hands[index], frameData))
-    );
-    if (frameBuffers.every(buffer => buffer instanceof Buffer)) {
-        return frameBuffers;
-    } else {
-        logger.error(`生成 GIF 时发生错误: 解析帧数据时发生错误`);
+    for(let i=0;i<hands.length;i++) {
+        const src = hands[i]
+        const frame = frames[i]
+        const join:ComposeJoin[] = [
+            {img:src,frameData:{}},
+            {img:inputImg,frameData:{...frame,blendOption:"dest-over"}}
+        ]
+        const result_ = await tools.imageTools.compose(join)
+        result.push(result_)
     }
-}
+    const gif = await tools.gifTools.pngsToGifBuffer_ffmpeg(result)
+    return gif
+})
 
 // 处理 GIF 图像
-const processGifImage = timeIt(async function processGifImage(inputImg: Buffer): Promise<Buffer[] | undefined> {
-    // 生成新的帧数据
-    const generateNewFrames = timeIt(function generateNewFrames(targetCount: number, handImgs: number): FrameData[] {
-        const forTime = targetCount / handImgs;
-        const newFrames: FrameData[] = [];
-
-        for (let i = 0; i < forTime; i++) {
-            newFrames.push(...frames);
-        }
-        return newFrames;
-    })
-    const _frameData = frames
-    const _handCount = BASE_DATA.frameCounts
+const processGifImage = timeIt(async function processGifImage(inputImg: Buffer): Promise<Buffer> {
+    const result:Buffer[] = []
     const hands = await loadHandImages()
-    const [_hands,_inputs,_rt] = await tools.gifTools.align2Gif(hands,inputImg)
-    const _frameBuffers = await Promise.all(
-        _hands.map(async(hand,index)=>{
-            const handIndex = index % _handCount
-            return createFrame(_inputs[index],hand,_frameData[handIndex])
-        })
-    )
-    return _frameBuffers
+    const [hands1, input2] = await tools.gifTools.align2Gif(hands, inputImg)
+    for(let i=0;i<hands1.length;i++) {
+        const src = hands1[i]
+        const index = i%frames.length
+        const frame = frames[index]
+        const join:ComposeJoin[] = [
+            {img:src, frameData:{}},
+            {img:input2[i],frameData:{...frame,blendOption:"dest-over"}}
+        ]
+        const result_ = await tools.imageTools.compose(join)
+        result.push(result_)
+    }
+    const gif = await tools.gifTools.pngsToGifBuffer_ffmpeg(result)
+    return gif
 })
 
 export default craftPetpetGif
-
-
-
-// 工具函数区
-function gcd(a: number, b: number): number {
-    // 使用欧几里得算法计算最大公约数
-    return b === 0 ? a : gcd(b, a % b);
-}
-
-function formatDuration(timeMark: number, nowTime: number): string {
-    // 计算时间差（毫秒）
-    const diffMs = Math.abs(timeMark - nowTime);
-
-    // 转换为秒
-    const seconds = Math.floor(diffMs / 1000) % 60;
-    // 转换为分钟
-    const minutes = Math.floor(diffMs / (1000 * 60)) % 60;
-    // 转换为小时
-    const hours = Math.floor(diffMs / (1000 * 60 * 60)) % 24;
-
-    // 格式化输出
-    return `${String(hours).padStart(2, '0')}时${String(minutes).padStart(2, '0')}分${String(seconds).padStart(2, '0')}秒`;
-}
